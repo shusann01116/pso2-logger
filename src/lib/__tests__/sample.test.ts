078e9fb6127e4f7d18a17ef6bf9fe4cc7e586225
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-07 09:43:45 +0900

    Format

--- src/lib/__tests__/sample.test.ts
+++ src/lib/__tests__/sample.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from 'vitest';
+import { expect, test } from "vitest";
 
-test('it works', () => {
+test("it works", () => {
   const result = 1 + 1;
   expect(result).toBe(2);
 });
