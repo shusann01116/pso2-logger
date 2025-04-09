120d2c54847c982da1836045d6c21f57a6ab4e86
shusann01116 <shusann@example.com>
2025-04-10 00:26:40 +0900

    Add restore and remove decorations, shadows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::WebviewUrl;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +9,19 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = tauri::WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                .transparent(true)
+                .build()
+                .unwrap();
+            window.set_decorations(false).unwrap();
+            window.set_shadow(false).unwrap();
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { useEffect, useRef } from "react";
+
 export default function Home() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    ref.current.onmousedown = async () => {
+      await getCurrentWindow().startDragging();
+    };
+  });
+
   return (
-    <div className="container">
+    <div className="container" ref={ref}>
       <h1 className="text-5xl font-bold">PSO2 Logger</h1>
       <p className="font-light">styled w/TailwindCSS</p>
     </div>
